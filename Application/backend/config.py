8e394853686656d91fe4f82a0bef4ee06c417663
amittkulkarni <amitcoolkarni@outlook.com>
2025-03-27 16:45:58 +0530

    Fixed config

--- Application/backend/config.py
+++ Application/backend/config.py
@@ -8,6 +8,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', '54641a3196e719a01144bad5cf0d22ac500d71caa11c035688ee510cbf6001f1')
     
